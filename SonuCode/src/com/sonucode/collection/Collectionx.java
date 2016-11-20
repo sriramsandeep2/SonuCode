99f5bd9bcd8b83e70bc997e8babf108fadf064bb
sriram sandeep <sriramsandeep2@gmail.com>
2016-11-20 17:51:06 -0600

    Checking

---
+++ SonuCode/src/com/sonucode/collection/Collectionx.java
@@ -0,0 +1,12 @@
+package com.sonucode.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collectionx {
+	public static void main(String[] args) {
+		List<String> al = new ArrayList<String>();
+		al.add("");
+	}
+
+}
